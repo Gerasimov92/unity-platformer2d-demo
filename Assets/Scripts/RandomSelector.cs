6bfe27231bdf039218f0386fb84fdbcc1b21cbb6
Ivan Gerasimov <aka.montoya@gmail.com>
2021-03-14 20:00:45 +0300

    Add random objects in BlockRoom

---
+++ Assets/Scripts/RandomSelector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RandomSelector : MonoBehaviour
+{
+    public int countToLeave = 1;
+
+    private void Start()
+    {
+        int count = Random.Range(0, 2) * countToLeave;
+        while (transform.childCount > count)
+        {
+            Transform childToDestroy = transform.GetChild(Random.Range(0, transform.childCount));
+            DestroyImmediate(childToDestroy.gameObject);
+        }
+    }
+}
